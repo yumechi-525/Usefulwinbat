0aebac57bd1c22c0b4f74d24b1f1611521d300dc
yumechi <yumechi@users.noreply.github.com>
2015-04-18 17:08:35 +0900

    Closing way changed. Before: We wait 10 seconds After: We input enter key

--- UserWritingCodeCount.bat
+++ UserWritingCodeCount.bat
@@ -1,8 +1,10 @@
 REM This bat need awk command
 REM If you using Windows OS, You install gawk
+echo off
 git log --numstat --pretty="%%H" | awk "NF==3 {plus+=$1; minus+=$2} END {printf(\"+%%d, -%%d, %%d\n\", plus, minus, plus - minus)}"
 :: If we specify one user
 :: git log --numstat --pretty="%%H" --author="yumechi-525" | awk "NF==3 {plus+=$1; minus+=$2} END {printf(\"+%%d, -%%d, %%d\n\", plus, minus, plus - minus)}"
 
-:: show 10 seconds
-timeout 10
+echo.
+:: showing result until input enter key
+set /P STR="If you input enter key, this bat will close."
